28189167c976d5ddb23e82f20a8d3881d229801a
Victor Romero <victorromero7005@outlook.com>
2024-02-14 14:25:07 -0700

    El pepe

--- MiPrimerChamba.EN/Categoria.cs
+++ MiPrimerChamba.EN/Categoria.cs
@@ -9,5 +9,6 @@ namespace MiPrimerChamba.EN
     public class Categoria
     {
         public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
--- MiPrimerChamba.EN/obj/Debug/net6.0/MiPrimerChamba.EN.AssemblyInfo.cs
+++ MiPrimerChamba.EN/obj/Debug/net6.0/MiPrimerChamba.EN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPrimerChamba.EN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66de19bfa65e8c28bce225318bf78716b9e717eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPrimerChamba.EN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPrimerChamba.EN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
