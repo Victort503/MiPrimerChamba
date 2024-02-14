66de19bfa65e8c28bce225318bf78716b9e717eb
Victor Romero <victorromero7005@outlook.com>
2024-02-14 13:31:46 -0700

    Mi primera chamba

---
+++ MiPrimerChamba.EN/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerChamba.EN
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+    }
+}
